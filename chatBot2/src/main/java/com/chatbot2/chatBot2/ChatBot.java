cce4549963783b8e40cedd88b3b301561c2125c1
roberto cassino <robertocassino@student.griffith.ie>
2022-03-27 23:19:57 +0100

    Pushing initial Chatbot skeleton with JUnit Test empty for development

---
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/ChatBot.java
@@ -0,0 +1,6 @@
+package com.chatbot2.chatBot2;
+
+public class ChatBot 
+{
+	//	Initial ChatBot
+}
---
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/ChatBotTest.java
@@ -0,0 +1,16 @@
+package com.chatbot2.chatBot2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChatBotTest 
+{
+	//	Initial test for ChatBot Class
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
