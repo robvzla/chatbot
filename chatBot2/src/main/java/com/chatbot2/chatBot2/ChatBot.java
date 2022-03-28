8b95445b765616dff90fd89c1a9f595d0710715e
Fiona Gacquin <fionagacquin@live.co.uk>
2022-03-28 11:37:18 +0100

    Add the message attribut to ChatBot class

--- chatBot2/src/main/java/com/chatbot2/chatBot2/ChatBot.java
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/ChatBot.java
@@ -1,6 +1,17 @@
 package com.chatbot2.chatBot2;
 
+import java.util.HashMap;
+
 public class ChatBot 
 {
-	//	Initial ChatBot
+	// Attributes
+	private static final String greeting = "Hi, I heard you're going on holiday? Do you want some help planning your wardrobe?";
+
+
+	public static void main(String[] args) {
+		// This is just for testing so the class can run. 
+		System.out.println("Hello");
+	}
+
+
 }
