9ed787b0fca1e8718424d7638bba1be94b202a64
Fiona Gacquin <fionagacquin@live.co.uk>
2022-03-28 11:41:13 +0100

    Add getter for greeting String

--- chatBot2/src/main/java/com/chatbot2/chatBot2/ChatBot.java
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/ChatBot.java
@@ -8,6 +8,10 @@ public class ChatBot
 	private static final String greeting = "Hi, I heard you're going on holiday? Do you want some help planning your wardrobe?";
 
 
+	// Add getters for testing purposes
+	public String getGreeting() {
+		return this.greeting;
+	}
 	public static void main(String[] args) {
 		// This is just for testing so the class can run. 
 		System.out.println("Hello");
