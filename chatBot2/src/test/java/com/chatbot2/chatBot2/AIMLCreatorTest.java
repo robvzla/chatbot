41b6372a2a00f575ed1f934dadf0a42a43e1820f
Fiona Gacquin <fionagacquin@live.co.uk>
2022-04-07 21:00:47 +0100

    Commit the AIMLCreatorTest file - empty for now

---
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/AIMLCreatorTest.java
@@ -0,0 +1,14 @@
+package com.chatbot2.chatBot2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AIMLCreatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
