e9c9b73d9551a68197e9389517c3183d3e3517a4
Fiona Gacquin <fionagacquin@live.co.uk>
2022-04-07 20:58:24 +0100

    Create the AIMLCreator Class - empty for now

---
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/AIMLCreator.java
@@ -0,0 +1,10 @@
+package com.chatbot2.chatBot2;
+
+public class AIMLCreator {
+	
+	// This class is going to use .txt files to generate AIML files 
+	// The .txt files will be stored in the config directory in the resources directory
+	// This class will be primarily used to generate an AIML file of locations for the chatbot
+	
+
+}
