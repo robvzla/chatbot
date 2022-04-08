e3b4e03029c1d815c0cc5007269df89cf7104873
Fiona Gacquin <fionagacquin@live.co.uk>
2022-04-08 12:07:54 +0100

    Add the main class - where all the code will be run

--- chatBot2/src/main/java/com/chatbot2/chatBot2/Main.java
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/Main.java
@@ -7,5 +7,21 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		// Main class for the chatbot
+		
+		
+		// Create the new AIML file
+		AIMLCreator newFile = new AIMLCreator("citylist.txt"); 
+		newFile.readFile();
+		newFile.writeFile();
+		
+		// Create the Chat Bot
+		ChatBot bot = new ChatBot(); 
+		
+		// Run the greeting method
+		bot.greeting(); 
+		
+		// Then run the askBot Method - this is where all the user input will be taken and stored
+		bot.AskBot(); 
+		
 	}
 }
