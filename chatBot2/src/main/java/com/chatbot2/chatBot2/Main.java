5a48c767c72366d86a47457471ba51967939a2c1
Fiona Gacquin <fionagacquin@live.co.uk>
2022-04-08 11:56:03 +0100

    Add the main file for the ChatBot

---
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/Main.java
@@ -0,0 +1,11 @@
+package com.chatbot2.chatBot2;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Main class for the chatbot
+	}
+}
