c85b8f0e200880413f14dacd954008bb00edf168
roberto cassino <robertocassino@student.griffith.ie>
2022-04-11 16:15:43 +0100

    Adding empty JUnit test for GeoLocator Class

---
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/GeoLocatorTest.java
@@ -0,0 +1,14 @@
+package com.chatbot2.chatBot2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GeoLocatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
