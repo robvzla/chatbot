a2bd2f2b97b477687072f8bbc097dcce31ecbb65
roberto cassino <robertocassino@student.griffith.ie>
2022-04-11 16:28:26 +0100

    Adding Test Case for DataRequest()

--- chatBot2/src/test/java/com/chatbot2/chatBot2/GeoLocatorTest.java
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/GeoLocatorTest.java
@@ -2,13 +2,23 @@ package com.chatbot2.chatBot2;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+
 import org.junit.Test;
+
 
 public class GeoLocatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDataRequest() throws MalformedURLException
+	{
+		//	Dublin coordinates
+		GeoLocator geoLocator = new GeoLocator("Dublin");
+		StringBuilder  actual = geoLocator.DataRequest();
+		boolean isEmpty = actual.toString().isEmpty();
+		assertFalse(isEmpty);
 	}
+	
+	
 
 }
