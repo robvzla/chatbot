07fbc1b93be32c3a169a26b1ac76068681fe98f2
roberto cassino <robertocassino@student.griffith.ie>
2022-04-11 16:36:24 +0100

    Adding attributes and constructor for Weather class

--- chatBot2/src/main/java/com/chatbot2/chatBot2/Weather.java
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/Weather.java
@@ -1,5 +1,25 @@
 package com.chatbot2.chatBot2;
 
-public class Weather {
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class Weather 
+{
+	//	Attributes
+	private final String BODY = "https://api.openweathermap.org/data/2.5/onecall?lat=";
+	private final String LATITUDE;
+	private final String LONGITUDE;
+	private final String EXCLUDE = "&exclude=current,minutely,hourly,alerts";
+	private final String API_KEY = "&appid=8fe0a86b1d08f7b8246e13ca39cf3b4e";
+	private final String UNITS = "&units=metric";
+	private URL url;
+	private StringBuilder result;
+
+	//	constructor
+	public Weather(String latitude, String longitude) throws MalformedURLException 
+	{
+		this.LATITUDE = latitude;
+		this.LONGITUDE = "&lon="+longitude;
+		this.url = new URL(BODY+LATITUDE+LONGITUDE+EXCLUDE+API_KEY+UNITS);
+	}
 }
