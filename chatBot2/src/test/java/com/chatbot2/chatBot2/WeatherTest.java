9452c6547014739a90c3686fbbc40f79185d78b8
roberto cassino <robertocassino@student.griffith.ie>
2022-04-11 16:35:58 +0100

    Adding Test Case for DataRequest(), not passing

--- chatBot2/src/test/java/com/chatbot2/chatBot2/WeatherTest.java
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/WeatherTest.java
@@ -2,13 +2,22 @@ package com.chatbot2.chatBot2;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+
 import org.junit.Test;
 
-public class WeatherTest {
+
+public class WeatherTest 
+{
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDataRequest() throws MalformedURLException
+	{
+		//	Dublin coordinates
+		Weather weather = new Weather("53.3498", "-6.2603");
+		StringBuilder actual = weather.DataRequest();
+		boolean isEmpty = actual.toString().isEmpty();
+		assertFalse(isEmpty);
 	}
 
 }
