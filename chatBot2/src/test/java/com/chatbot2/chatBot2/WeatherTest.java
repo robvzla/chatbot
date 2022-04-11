296d423dcec7aec1aaa4646d4fff0cf227a8609e
roberto cassino <robertocassino@student.griffith.ie>
2022-04-11 16:38:44 +0100

    Adding Test Case for ForecastTemperature() - not passing

--- chatBot2/src/test/java/com/chatbot2/chatBot2/WeatherTest.java
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/WeatherTest.java
@@ -3,8 +3,11 @@ package com.chatbot2.chatBot2;
 import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
+
+import com.chatbot1.chatBot1.Weather;
 
 
 public class WeatherTest 
@@ -18,6 +21,17 @@ public class WeatherTest
 		StringBuilder actual = weather.DataRequest();
 		boolean isEmpty = actual.toString().isEmpty();
 		assertFalse(isEmpty);
+	}
+	
+	@Test
+	public void testForecastTemperatures() throws MalformedURLException
+	{
+		//	Dublin coordinates
+		Weather weather = new Weather("53.3498", "-6.2603");
+		int  expected = 8;
+		ArrayList<String> temperatures = weather.ForecastTemperatures();
+		int actual = temperatures.size();
+		assertEquals(expected, actual);
 	}
 
 }
