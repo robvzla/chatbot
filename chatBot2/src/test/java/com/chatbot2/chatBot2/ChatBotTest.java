1ffa6acce34d597137c6a5825cf9ea53de86169b
Fiona Gacquin <fionagacquin@live.co.uk>
2022-04-12 11:06:24 +0100

    Removed all the tests that require user input and put them in their own test file.

--- chatBot2/src/test/java/com/chatbot2/chatBot2/ChatBotTest.java
+++ chatBot2/src/test/java/com/chatbot2/chatBot2/ChatBotTest.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import org.junit.Test;
 
 
-public class ChatBotTest 
+public class ChatBotTest
 {
 	// Test greeting method
 
@@ -31,21 +31,13 @@ public class ChatBotTest
 	// Test Two: 
 	// Test to check that yes is accepted, boolean should return true
 	// The greeting method gives the user an option to proceed with the bot
-	@Test
-	public void greetingMethodTestForYesOrY() {
-		ChatBot bot = new ChatBot();
-		boolean b = bot.greeting(); // type yes or y for the boolean to be true
-		assertTrue(b);
-	}
+	// - MOVED TO ChatBotTest2UserInput.java as this test requires userInput
+	
 
 	// Test Three: 
 	// Test to check that no is accepted, boolean should return false
-	@Test
-	public void greetingMethodTestForNoOrN() {
-		ChatBot bot = new ChatBot();
-		boolean b = bot.greeting(); // type anything but yes or y for a false or for immediate false type no or n
-		assertFalse(b);
-	}
+	// - MOVED TO ChatBotTest2UserInput.java as this test requires userInput
+	
 
 	/*
 	 * This has been made redundant as we are no longer using a HashMap but are using a 2d ArrayList instead
@@ -221,20 +213,9 @@ public class ChatBotTest
 	/*
 	 * Test One: 
 	 * Test to see if I get back the correct string that I type in 
+	 * // - MOVED TO ChatBotTest2UserInput.java as this test requires userInput
 	 */
-	@Test
-	public void testAskMethodGetCorrectStringReturned() {
-		ChatBot bot = new ChatBot(); 
-		String actual = ""; 
-		try {
-			actual = bot.AskBot();  // Type Hello into the console and then go back to see if it is a green/red bar result
-		} catch (IOException e) {
-			fail(); 
-		}
-		String expected = "Hello"; 
-		assertEquals(actual, expected); 
-
-	}
+	
 	
 	/*
 	 * checkArrayLocation()
@@ -353,14 +334,8 @@ public class ChatBotTest
 	
 	/*
 	 * Test Nine: 
-	 */
-	@Test
-	public void testWhatDate() {
-		ChatBot bot = new ChatBot(); 
-		String actual = bot.whatDate(); // Type 15/11/2022 to get a green bar
-		String expected = "15/11/2022";
-		assertEquals(actual, expected); 
-	}
+	 * - MOVED TO ChatBotTest2UserInput.java as this test requires userInput
+	 */ 
 	
 	
 	/*
@@ -455,7 +430,9 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate1() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16/06/2022")); 
+		String actual = bot.returnValidDate("16/06/2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
 	
 	/*
@@ -465,8 +442,11 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate2() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16-06-2022")); 
+		String actual = bot.returnValidDate("16-06-2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
+	
 	
 	/*
 	 * Test Nineteen: 
@@ -475,8 +455,11 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate3() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16.06.2022")); 
+		String actual = bot.returnValidDate("16.06.2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
+	
 	
 	/*
 	 * Test Twenty: 
@@ -485,7 +468,9 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate4() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16/06.2022")); 
+		String actual = bot.returnValidDate("16/06.2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
 	
 	/*
@@ -495,7 +480,9 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate5() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16-06.2022")); 
+		String actual = bot.returnValidDate("16-06.2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
 	
 	/*
@@ -505,7 +492,9 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate6() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16.06-2022")); 
+		String actual = bot.returnValidDate("16.06-2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
 	
 	/*
@@ -515,8 +504,11 @@ public class ChatBotTest
 	@Test
 	public void testReturnValidDate7() {
 		ChatBot bot = new ChatBot(); 
-		assertTrue(bot.dateValidation("16/06-2022")); 
+		String actual = bot.returnValidDate("16/06-2022"); 
+		String expected = "16-06-2022"; 
+		assertEquals(expected, actual); 
 	}
+	
 	
 	/*
 	 * addDateToArrayList() 
@@ -548,27 +540,16 @@ public class ChatBotTest
 	/*
 	 * Test Twenty-Five: 
 	 * Test by answering 'yes'. Should return false; 
+	 * - MOVED TO ChatBotTest2UserInput.java as this test requires userInput
 	 */
 
-	@Test
-	public void testExitLoopFalse() throws MalformedURLException, ParseException{
-		ChatBot bot = new ChatBot();
-		boolean isFalse = false; 
-		isFalse = bot.exitLoop(); // Type yes into console to test
-		assertFalse(isFalse); 	
-	}
 	
 	/*
 	 * Test Twenty-Six: 
 	 * Test by answering 'no'. Should return true
+	 * - MOVED TO ChatBotTest2UserInput.java as this test requires userInput
 	 */
-	@Test
-	public void testExitLoopTrue() throws MalformedURLException, ParseException{
-		ChatBot bot = new ChatBot();
-		boolean isTrue = false; 
-		isTrue = bot.exitLoop(); // Type no into console to test
-		assertTrue(isTrue); 	
-	}
+
 	
 	/*
 	 * Test Case: Checks if days differences is calculated accurately. OpenWeatherMap API can only
