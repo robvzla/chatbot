a61986938b8ca0a7e2e76276ce564ffd6b44e83a
Fiona Gacquin <fionagacquin@live.co.uk>
2022-05-06 21:47:33 +0100

    Refactor the main method, remove the unsed int and then take the bot.runBot() out of the do while loop

--- chatBot2/src/main/java/com/chatbot2/chatBot2/Main.java
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/Main.java
@@ -3,27 +3,24 @@ package com.chatbot2.chatBot2;
 import java.io.IOException;
 import java.text.ParseException;
 
-public class Main {
+public class Main{
+
 	public static void main(String[] args) throws IOException, ParseException {
-		
+
+
 		// Create the aiml file from the citylist.txt file
 		AIMLCreator newFile = new AIMLCreator("citylist.txt");
 		newFile.readFile(); 
 		newFile.writeFile();
-		
+
 		// Create the bot
 		ChatBot bot = new ChatBot(); 
-		int i = 0; 
-		
+
 		// Run the greeting method
 		if(bot.greeting()) {
-			do {
-				bot.runBot();
-			} while (!bot.runBot()); 
+			bot.runBot();
 		}
-//		System.out.println("Show the list of locations and dates: ");
-//		bot.displayList(); 
-		// Run the askBot method
 	}
-
 }
+
+
