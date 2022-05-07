15aabe6ab51eaa5dd2927014741fc8f29f186aa0
Fiona Gacquin <fionagacquin@live.co.uk>
2022-05-07 18:04:13 +0100

    Add the required attributes and the methods from the interface

--- chatBot2/src/main/java/com/chatbot2/chatBot2/SwingGUI.java
+++ chatBot2/src/main/java/com/chatbot2/chatBot2/SwingGUI.java
@@ -1,7 +1,33 @@
 package com.chatbot2.chatBot2;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class SwingGUI implements ActionListener{
 
+	// Attributes
+	private ChatBot bot; 
+	private JButton button;
+	private JButton btn2; 
+	private JLabel output; 
+	private JTextField userInput; 
+	private JFrame frame; 
+	private JPanel panel; 
+	private JPanel p2;
+	
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	} 
 }
